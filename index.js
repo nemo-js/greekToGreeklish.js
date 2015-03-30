f2eab816ccd9f26e0b806bc8baabb08259ee860e
Dimitris Moudilos <d.moudilos@clmsuk.com>
2015-03-30 08:06:50 +0300

    initial (empty) implementation

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    /**
+     * Convert greekString to Greekglish.
+     *
+     * @param  {String} greekString
+     * @return {String}
+     */
+    convert: function (greekString, overrides) {
+        overrides = overrides || {};
+        return greekString;
+    }
+
+};
