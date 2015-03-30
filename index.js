f8b1991306ae7bc1de30ff7c90eee207d410e12d
Dimitris Moudilos <d.moudilos@clmsuk.com>
2015-03-30 08:07:28 +0300

    'use strict'

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ module.exports = {
      * @return {String}
      */
     convert: function (greekString, overrides) {
+        'use strict';
+
         overrides = overrides || {};
         return greekString;
     }
